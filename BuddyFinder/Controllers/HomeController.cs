020c58e32eb5d682413b295479cf11564cca4bd9
Radu B <radu.buciuceanu@gmail.com>
2018-11-14 11:22:46 +0200

    Namespaces were adjusted fron BuddyFinder.Frontend to BuddyFinder one.

--- BuddyFinder/Controllers/HomeController.cs
+++ BuddyFinder/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BuddyFinder.Frontend.Controllers
+namespace BuddyFinder.Controllers
 {
     public class HomeController : Controller
     {

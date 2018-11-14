78f06bd460ea537988c599f8108c697d25f52785
Radu B <radu.buciuceanu@gmail.com>
2018-11-14 19:57:51 +0200

    Pages fetch data and counter were removed.

--- BuddyFinder/ClientApp/src/app/app.module.ts
+++ BuddyFinder/ClientApp/src/app/app.module.ts
@@ -11,8 +11,6 @@ import {MaterialModule} from '../material/material.module';
 import {AllUsersComponent} from './all-users/all-users.component';
 import {AppComponent} from './app.component';
 import {ContainerComponent} from './container/container.component';
-import {CounterComponent} from './counter/counter.component';
-import {FetchDataComponent} from './fetch-data/fetch-data.component';
 import {HomeComponent} from './home/home.component';
 import {NavMenuComponent} from './nav-menu/nav-menu.component';
 import {PageContentComponent} from './page-content/page-content.component';
@@ -22,17 +20,15 @@ import {UserDetailsComponent} from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent, NavMenuComponent, HomeComponent, CounterComponent,
-    FetchDataComponent, AllUsersComponent, SectionComponent, PageTitleComponent,
-    UserDetailsComponent, ContainerComponent, PageContentComponent
+    AppComponent, NavMenuComponent, HomeComponent, AllUsersComponent,
+    SectionComponent, PageTitleComponent, UserDetailsComponent,
+    ContainerComponent, PageContentComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     BrowserAnimationsModule, FormsModule, HttpClientModule, MaterialModule,
     ReactiveFormsModule, RouterModule.forRoot([
       {path: '', component: HomeComponent, pathMatch: 'full'},
-      {path: 'counter', component: CounterComponent},
-      {path: 'fetch-data', component: FetchDataComponent},
       {path: 'all-users', component: AllUsersComponent}
     ])
   ],

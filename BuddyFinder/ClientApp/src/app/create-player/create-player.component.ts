8e2608ee2348f6ad3841dd63824b9b876760f64f
Radu B <radu.buciuceanu@gmail.com>
2018-11-15 16:07:02 +0200

    In create player view, input fields were added to the form.

--- BuddyFinder/ClientApp/src/app/create-player/create-player.component.ts
+++ BuddyFinder/ClientApp/src/app/create-player/create-player.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PlayerRepository } from '../repositories/player-repository';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'bf-create-player',
@@ -7,5 +8,32 @@ import { PlayerRepository } from '../repositories/player-repository';
     styleUrls: ['./create-player.component.css']
 })
 export class CreatePlayerComponent {
-    constructor(private playerRepository: PlayerRepository) { }
+    readonly formControl: FormControl;
+
+    firstName: string;
+    lastName: string;
+    age: string;
+    height: string;
+    weight: string;
+    experience: string;
+    matchesPerMonth: string;
+
+    constructor(private playerRepository: PlayerRepository) {
+        this.formControl = new FormControl('', [Validators.required]);
+    }
+
+    get canSave(): boolean {
+        return this.isValid(this.firstName) &&
+            this.isValid(this.lastName) &&
+            this.isValid(this.age) &&
+            this.isValid(this.height) &&
+            this.isValid(this.weight) &&
+            this.isValid(this.experience) &&
+            this.isValid(this.matchesPerMonth) &&
+            !this.formControl.hasError('required');
+    }
+
+    private isValid(value: string): boolean {
+        return value !== undefined && value !== '';
+    }
 }

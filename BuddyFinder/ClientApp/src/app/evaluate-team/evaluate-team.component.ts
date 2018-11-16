a873969daf5a41c22d3c0c458c3a4e2c1f2df6ca
Radu B <radu.buciuceanu@gmail.com>
2018-11-16 06:55:45 +0200

    Evaluate team, empty page was created.

---
+++ BuddyFinder/ClientApp/src/app/evaluate-team/evaluate-team.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'bf-evaluate-team',
+    templateUrl: './evaluate-team.component.html',
+    styleUrls: ['./evaluate-team.component.css']
+})
+export class EvaluateTeamComponent {
+
+}
